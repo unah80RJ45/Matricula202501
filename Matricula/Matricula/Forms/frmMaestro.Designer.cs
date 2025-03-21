168f258dc7ddd791e613946c88be621420f041a9
unah80RJ45 <guillermo.borjas@unah.edu.hn>
2025-03-20 19:54:47 -0600

    Clase 20 Marzo

---
+++ Matricula/Matricula/Forms/frmMaestro.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Matricula.Forms
+{
+    partial class frmMaestro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cmdSalvar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 388);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // cmdSalvar
+            // 
+            this.cmdSalvar.Location = new System.Drawing.Point(12, 411);
+            this.cmdSalvar.Name = "cmdSalvar";
+            this.cmdSalvar.Size = new System.Drawing.Size(259, 80);
+            this.cmdSalvar.TabIndex = 1;
+            this.cmdSalvar.Text = "Salvar";
+            this.cmdSalvar.UseVisualStyleBackColor = true;
+            this.cmdSalvar.Click += new System.EventHandler(this.cmdSalvar_Click);
+            // 
+            // frmMaestro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 503);
+            this.Controls.Add(this.cmdSalvar);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmMaestro";
+            this.Text = "frmMaestro";
+            this.Load += new System.EventHandler(this.frmMaestro_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button cmdSalvar;
+    }
+}
---
+++ Matricula/Matricula/Forms/frmMaestro.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Matricula.Forms
+{
+    public partial class frmMaestro : Form
+    {
+        SqlConnection cnx;
+        SqlDataAdapter adpMaestro;
+        DataTable tabMaestro;
+
+        public frmMaestro()
+        {
+            InitializeComponent();
+
+            Conexion();
+        }
+
+        private void Conexion()
+        {
+            String url = "Server=3.128.144.165;Database=Matricula202501;User Id=db2;Password=123;";
+            cnx = new SqlConnection(url);
+            adpMaestro = new SqlDataAdapter();
+
+            adpMaestro.SelectCommand = new SqlCommand("spMaestroSelect", cnx);
+            adpMaestro.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+            adpMaestro.InsertCommand = new SqlCommand("spMaestroInsert", cnx);
+            adpMaestro.InsertCommand.CommandType = CommandType.StoredProcedure;
+            adpMaestro.InsertCommand.Parameters.Add("@mae", SqlDbType.Int, 4, "MaestroID");
+            adpMaestro.InsertCommand.Parameters.Add("@nom", SqlDbType.VarChar, 50, "Nombre");
+            adpMaestro.InsertCommand.Parameters.Add("@sex", SqlDbType.VarChar, 1, "Sexo");
+            adpMaestro.InsertCommand.Parameters.Add("@civ", SqlDbType.VarChar, 1, "civil");
+            adpMaestro.InsertCommand.Parameters.Add("@tel", SqlDbType.VarChar, 20, "telefono");
+            adpMaestro.InsertCommand.Parameters.Add("@dir", SqlDbType.VarChar, 100, "Direccion");
+            adpMaestro.InsertCommand.Parameters.Add("@pro", SqlDbType.Int, 4, "ProfesionID");
+
+            adpMaestro.UpdateCommand = new SqlCommand("spMaestroUpdate", cnx);
+            adpMaestro.UpdateCommand.CommandType = CommandType.StoredProcedure;
+            adpMaestro.UpdateCommand.Parameters.Add("@mae", SqlDbType.Int, 4, "MaestroID");
+            adpMaestro.UpdateCommand.Parameters.Add("@nom", SqlDbType.VarChar, 50, "Nombre");
+            adpMaestro.UpdateCommand.Parameters.Add("@sex", SqlDbType.VarChar, 1, "Sexo");
+            adpMaestro.UpdateCommand.Parameters.Add("@civ", SqlDbType.VarChar, 1, "civil");
+            adpMaestro.UpdateCommand.Parameters.Add("@tel", SqlDbType.VarChar, 20, "telefono");
+            adpMaestro.UpdateCommand.Parameters.Add("@dir", SqlDbType.VarChar, 100, "Direccion");
+            adpMaestro.UpdateCommand.Parameters.Add("@pro", SqlDbType.Int, 4, "ProfesionID");
+
+            adpMaestro.DeleteCommand = new SqlCommand();
+            adpMaestro.DeleteCommand.CommandText = "spMaestroDelete";
+            adpMaestro.DeleteCommand.CommandType = CommandType.StoredProcedure;
+            adpMaestro.DeleteCommand.Connection = cnx;
+            adpMaestro.DeleteCommand.Parameters.Add("@mae", SqlDbType.Int, 4, "MaestroID");
+        }
+
+        private void frmMaestro_Load(object sender, EventArgs e)
+        {
+            tabMaestro = new DataTable();
+            adpMaestro.Fill(tabMaestro);
+            dataGridView1.DataSource = tabMaestro;
+        }
+
+        private void cmdSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                adpMaestro.Update(tabMaestro);
+                MessageBox.Show("Datos salvados satifactoriamente", "Salvar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
