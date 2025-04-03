840854b18f38e5ebbc7082a1ffe8cab9805852f0
unah80RJ45 <guillermo.borjas@unah.edu.hn>
2025-04-02 19:49:17 -0600

    Clase Abril 2

--- Matricula/Matricula/Forms/frmSeccion.Designer.cs
+++ Matricula/Matricula/Forms/frmSeccion.Designer.cs
@@ -29,16 +29,16 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.cmdMateria = new System.Windows.Forms.ComboBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.cmbMateria = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.cmbMaestro = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
             this.cmbCarrera = new System.Windows.Forms.ComboBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtHorario = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtAula = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.cmdSalvar = new System.Windows.Forms.Button();
@@ -49,149 +49,167 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(62, 36);
+            this.label1.Location = new System.Drawing.Point(83, 44);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.Size = new System.Drawing.Size(52, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Codigo";
             // 
-            // textBox1
+            // txtCodigo
             // 
-            this.textBox1.Location = new System.Drawing.Point(119, 32);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtCodigo.Location = new System.Drawing.Point(159, 39);
+            this.txtCodigo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(132, 22);
+            this.txtCodigo.TabIndex = 1;
             // 
-            // cmdMateria
+            // cmbMateria
             // 
-            this.cmdMateria.FormattingEnabled = true;
-            this.cmdMateria.Location = new System.Drawing.Point(119, 66);
-            this.cmdMateria.Name = "cmdMateria";
-            this.cmdMateria.Size = new System.Drawing.Size(373, 21);
-            this.cmdMateria.TabIndex = 2;
+            this.cmbMateria.FormattingEnabled = true;
+            this.cmbMateria.Location = new System.Drawing.Point(159, 81);
+            this.cmbMateria.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbMateria.Name = "cmbMateria";
+            this.cmbMateria.Size = new System.Drawing.Size(496, 24);
+            this.cmbMateria.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(62, 70);
+            this.label2.Location = new System.Drawing.Point(83, 86);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.Size = new System.Drawing.Size(55, 17);
             this.label2.TabIndex = 3;
             this.label2.Text = "Materia";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(62, 105);
+            this.label3.Location = new System.Drawing.Point(83, 129);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.Size = new System.Drawing.Size(59, 17);
             this.label3.TabIndex = 5;
             this.label3.Text = "Maestro";
             // 
             // cmbMaestro
             // 
             this.cmbMaestro.FormattingEnabled = true;
-            this.cmbMaestro.Location = new System.Drawing.Point(119, 101);
+            this.cmbMaestro.Location = new System.Drawing.Point(159, 124);
+            this.cmbMaestro.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmbMaestro.Name = "cmbMaestro";
-            this.cmbMaestro.Size = new System.Drawing.Size(373, 21);
+            this.cmbMaestro.Size = new System.Drawing.Size(496, 24);
             this.cmbMaestro.TabIndex = 4;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(62, 140);
+            this.label4.Location = new System.Drawing.Point(83, 172);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.Size = new System.Drawing.Size(56, 17);
             this.label4.TabIndex = 7;
             this.label4.Text = "Carrera";
             // 
             // cmbCarrera
             // 
             this.cmbCarrera.FormattingEnabled = true;
-            this.cmbCarrera.Location = new System.Drawing.Point(119, 136);
+            this.cmbCarrera.Location = new System.Drawing.Point(159, 167);
+            this.cmbCarrera.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmbCarrera.Name = "cmbCarrera";
-            this.cmbCarrera.Size = new System.Drawing.Size(373, 21);
+            this.cmbCarrera.Size = new System.Drawing.Size(496, 24);
             this.cmbCarrera.TabIndex = 6;
             // 
-            // textBox2
+            // txtHorario
             // 
-            this.textBox2.Location = new System.Drawing.Point(119, 171);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 9;
+            this.txtHorario.Location = new System.Drawing.Point(159, 210);
+            this.txtHorario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtHorario.Name = "txtHorario";
+            this.txtHorario.Size = new System.Drawing.Size(132, 22);
+            this.txtHorario.TabIndex = 9;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(62, 175);
+            this.label5.Location = new System.Drawing.Point(83, 215);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.Size = new System.Drawing.Size(55, 17);
             this.label5.TabIndex = 8;
             this.label5.Text = "Horario";
             // 
-            // textBox3
+            // txtAula
             // 
-            this.textBox3.Location = new System.Drawing.Point(392, 171);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 11;
+            this.txtAula.Location = new System.Drawing.Point(523, 210);
+            this.txtAula.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtAula.Name = "txtAula";
+            this.txtAula.Size = new System.Drawing.Size(132, 22);
+            this.txtAula.TabIndex = 11;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(335, 175);
+            this.label6.Location = new System.Drawing.Point(447, 215);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.Size = new System.Drawing.Size(36, 17);
             this.label6.TabIndex = 10;
             this.label6.Text = "Aula";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(23, 207);
+            this.dataGridView1.Location = new System.Drawing.Point(31, 255);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(735, 300);
+            this.dataGridView1.Size = new System.Drawing.Size(980, 369);
             this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.dataGridView1_CellValidating);
             // 
             // cmdSalvar
             // 
-            this.cmdSalvar.Location = new System.Drawing.Point(530, 66);
+            this.cmdSalvar.Location = new System.Drawing.Point(707, 81);
+            this.cmdSalvar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmdSalvar.Name = "cmdSalvar";
-            this.cmdSalvar.Size = new System.Drawing.Size(228, 58);
+            this.cmdSalvar.Size = new System.Drawing.Size(304, 71);
             this.cmdSalvar.TabIndex = 13;
             this.cmdSalvar.Text = "Salvar";
             this.cmdSalvar.UseVisualStyleBackColor = true;
+            this.cmdSalvar.Click += new System.EventHandler(this.cmdSalvar_Click);
             // 
             // cmdCancelar
             // 
-            this.cmdCancelar.Location = new System.Drawing.Point(530, 125);
+            this.cmdCancelar.Location = new System.Drawing.Point(707, 154);
+            this.cmdCancelar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmdCancelar.Name = "cmdCancelar";
-            this.cmdCancelar.Size = new System.Drawing.Size(228, 58);
+            this.cmdCancelar.Size = new System.Drawing.Size(304, 71);
             this.cmdCancelar.TabIndex = 14;
             this.cmdCancelar.Text = "cmdCancelar";
             this.cmdCancelar.UseVisualStyleBackColor = true;
             // 
             // frmSeccion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(783, 519);
+            this.ClientSize = new System.Drawing.Size(1044, 639);
             this.Controls.Add(this.cmdCancelar);
             this.Controls.Add(this.cmdSalvar);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtAula);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtHorario);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.cmbCarrera);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.cmbMaestro);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.cmdMateria);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cmbMateria);
+            this.Controls.Add(this.txtCodigo);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmSeccion";
@@ -206,16 +224,16 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ComboBox cmdMateria;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.ComboBox cmbMateria;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox cmbMaestro;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ComboBox cmbCarrera;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtHorario;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtAula;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button cmdSalvar;
--- Matricula/Matricula/Forms/frmSeccion.cs
+++ Matricula/Matricula/Forms/frmSeccion.cs
@@ -15,12 +15,37 @@ namespace Matricula.Forms
     {
         SqlDataAdapter adpTablas;
         DataSet dsTablas;
+        SqlDataAdapter adpSeccion;
+        SqlDataAdapter adpClase;
+        SqlConnection conexion;
 
         public frmSeccion(SqlConnection cnx, int seccion)
         {
+            String spSeccion = "spSeccionSelect " + seccion;
+            String spClase = "exec spClaseSelect " + seccion;
+            conexion = cnx;
+
             InitializeComponent();
 
-            adpTablas = new SqlDataAdapter("spSeccionSelect 0; exec spClaseSelect 0; exec spMateriaSelect ''; exec spMaestroSelect; exec spCarreraSelect 0", cnx);
+            adpTablas = new SqlDataAdapter(spSeccion + ";" + spClase + "; exec spMateriaSelect ''; exec spMaestroSelect; exec spCarreraSelect 0", cnx);
+
+            adpSeccion = new SqlDataAdapter();
+            adpSeccion.InsertCommand = new SqlCommand("spSeccionInsert", cnx);
+            adpSeccion.InsertCommand.CommandType = CommandType.StoredProcedure;
+            adpSeccion.InsertCommand.Parameters.Add("@seccion", SqlDbType.Int, 4, "SeccionID");
+            adpSeccion.InsertCommand.Parameters.Add("@materia", SqlDbType.VarChar, 20, "MateriaID");
+            adpSeccion.InsertCommand.Parameters.Add("@maestro", SqlDbType.Int, 4, "MaestroID");
+            adpSeccion.InsertCommand.Parameters.Add("@carrera", SqlDbType.Int, 4, "CarreraID");
+            adpSeccion.InsertCommand.Parameters.Add("@aula", SqlDbType.Int, 4, "aula");
+            adpSeccion.InsertCommand.Parameters.Add("@horario", SqlDbType.VarChar , 20, "Aula");
+
+            adpClase = new SqlDataAdapter();
+            adpClase.InsertCommand = new SqlCommand("spClaseInsert", cnx);
+            adpClase.InsertCommand.CommandType = CommandType.StoredProcedure;
+            adpClase.InsertCommand.Parameters.Add("@seccion", SqlDbType.Int, 4, "SeccionID");
+            adpClase.InsertCommand.Parameters.Add("@alumno", SqlDbType.Int, 4, "AlumnoID");
+            adpClase.InsertCommand.Parameters.Add("@nota", SqlDbType.Int, 4, "Nota");
+
         }
         public frmSeccion()
         {
@@ -39,15 +64,68 @@ namespace Matricula.Forms
                 dsTablas = new DataSet();
                 adpTablas.Fill(dsTablas);
 
+                dsTablas.Tables[0].TableName = "Seccion";
                 dsTablas.Tables[1].TableName = "Clase";
+                dsTablas.Tables[2].TableName = "Materia";
+                dsTablas.Tables[3].TableName = "Maestro";
+                dsTablas.Tables[4].TableName = "Carrera";
 
                 dataGridView1.DataSource = dsTablas.Tables["Clase"];
                 initCombo(cmbCarrera, dsTablas.Tables[4], "Nombre", "CarreraID");
                 initCombo(cmbMaestro, dsTablas.Tables[3], "Nombre", "MaestroID");
+                initCombo(cmbMateria, dsTablas.Tables["Materia"], "Nombre", "MateriaID");
+
+                if (dsTablas.Tables["Seccion"].Rows.Count == 0)
+                    dsTablas.Tables["Seccion"].Rows.Add();
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void cmdSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dsTablas.Tables["Seccion"].Rows[0]["SeccionID"] = txtCodigo.Text;
+                dsTablas.Tables["Seccion"].Rows[0]["MateriaID"] = cmbMateria.SelectedValue;
+                dsTablas.Tables["Seccion"].Rows[0]["MaestroID"] = cmbMaestro.SelectedValue;
+                dsTablas.Tables["Seccion"].Rows[0]["CarreraID"] = cmbCarrera.SelectedValue;
+                dsTablas.Tables["Seccion"].Rows[0]["Horario"] = txtHorario.Text;
+                dsTablas.Tables["Seccion"].Rows[0]["Aula"] = txtAula.Text;
+
+                adpSeccion.Update(dsTablas.Tables["Seccion"]);
+                adpClase.Update(dsTablas.Tables["Clase"]);
+
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView1_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            String col = dataGridView1.Columns[e.ColumnIndex].Name.ToLower();
+
+            if(col == "alumnoid")
+            {
+                DataTable tabAlumno = new DataTable();
+                SqlDataAdapter adpAlumno = new SqlDataAdapter("spAlumnoSelect " + e.FormattedValue, conexion);
+                adpAlumno.Fill(tabAlumno);
+
+                if(tabAlumno.Rows.Count == 0)
+                {
+                    MessageBox.Show("El alumno no existe", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    e.Cancel = true;
+                }
+                else
+                {
+                    dsTablas.Tables["Clase"].DefaultView[e.RowIndex]["nombre"] = tabAlumno.Rows[0]["nombre"];
+                    dataGridView1.Refresh();
+                }
             }
         }
     }
